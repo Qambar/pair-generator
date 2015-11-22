9d91e858ddf4ec7eec7bc718ed47646d08bcec8b
Qambar Raza <Qambar@users.noreply.github.com>
2015-11-22 21:58:53 +0000

    added user service

---
+++ app/scripts/services/user.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name pairGeneratorApp.user
+ * @description
+ * # user
+ * Service in the pairGeneratorApp.
+ */
+angular.module('pairGeneratorApp')
+  .service('user', function () {
+
+    var _virtualIdCounter = 1;
+
+    var _users = [];
+
+    /**
+     * Adds a user to the list
+     * @param user
+     */
+    this.add = function (user) {
+
+      _validateUser(user);
+
+      if (_userExists(user)) {
+        throw new Error("User with the same name already exists");
+      }
+
+      user.id = _virtualIdCounter;
+      //Increase the virtual id counter
+      _virtualIdCounter = _virtualIdCounter + 1;
+
+      _users.push(user);
+
+      return user.id;
+    };
+
+    /**
+     * Deletes a user from the list
+     * @param user
+     */
+    this.delete = function(id) {
+
+      if (id > -1 && id < _virtualIdCounter) {
+        _users = _users.filter(function(u) {
+          return u.id !== id;
+        });
+
+        return true;
+      }
+
+      throw new Error("Invalid user id provided.");
+
+    };
+
+    /**
+     * @param id takes an id
+     * @returns user object
+     */
+    this.get = function(id) {
+      return _users.filter(function(u) {
+        return u.id === id;
+      });
+    };
+
+    /**
+     * Returns a list of all users
+     * @returns {Array}
+     */
+    this.getAll = function() {
+      return _users;
+    };
+
+
+    /**
+     * User object validation method
+     * @param user
+     * @private
+     */
+    function _validateUser(user) {
+      if (Object.keys(user).length == 0) {
+        throw new Error("User should have a name and type");
+      }
+
+      if (
+        angular.isUndefined(user.name)
+        || (user.name).trim() === ""
+      ) {
+        throw new Error("User should have a name");
+      }
+
+      if (
+        angular.isUndefined(user.type)
+        || (user.type).trim() === ""
+      ) {
+        throw new Error("User should have a type");
+      }
+
+    }
+
+    function _userExists(user) {
+      var result = _users.filter(function (u) {
+        return u.name === user.name;
+      });
+
+      return result.length > 0;
+    }
+
+  });
---
+++ test/spec/services/user.js
@@ -0,0 +1,149 @@
+'use strict';
+
+describe('Service: user', function () {
+
+  // load the service's module
+  beforeEach(module('pairGeneratorApp'));
+
+  // instantiate service
+  var user;
+
+  beforeEach(inject(function (_user_) {
+    user = _user_;
+  }));
+
+  it('should add user to the list', function () {
+    var userObj = {
+      "name" : "Qambar",
+      "type" : "Developer"
+    };
+
+    user.add(userObj);
+
+    var users = user.getAll();
+
+    expect(users[0].name).toBe(userObj.name);
+    expect(users[0].type).toBe(userObj.type);
+  });
+
+  it('should return id of user when added', function () {
+    var userObj = {
+      "name" : "Qambar",
+      "type" : "Developer"
+    };
+
+    expect(user.add(userObj)).toBe(1);
+  });
+
+  it('should throw error when object is empty', function () {
+    var userObj = {};
+
+    expect(function() {
+      user.add(userObj);
+    }).toThrow(
+      new Error("User should have a name and type")
+    );
+
+  });
+
+  it('should throw error when name is not defined', function () {
+    var userObj = {"type" : "developer"};
+
+    expect(function() {
+      user.add(userObj);
+    }).toThrow(
+      new Error("User should have a name")
+    );
+
+  });
+
+  it('should throw error when name is not defined', function () {
+    var userObj = {"name" : "Qambar"};
+
+    expect(function() {
+      user.add(userObj);
+    }).toThrow(
+      new Error("User should have a type")
+    );
+
+  });
+
+  it('should not let you add same user twice', function() {
+    var userObj = {
+      "name" : "Qambar",
+      "type" : "Developer"
+    };
+
+    user.add(userObj);
+
+    expect(function() {
+      user.add(userObj);
+    }).toThrow(
+      new Error("User with the same name already exists")
+    );
+
+  });
+
+  it('should let you add 2 different users', function() {
+    var user1Obj = {
+      "name" : "user1",
+      "type" : "Developer"
+    };
+    user.add(user1Obj);
+    expect((user.getAll()).length).toBe(1);
+
+    var user2Obj = {
+      "name" : "user2",
+      "type" : "Developer"
+    };
+    user.add(user2Obj);
+
+    expect((user.getAll()).length).toBe(2);
+
+  });
+
+  it ('should increase the virtual id counter when we add user', function() {
+    var user1Obj = {
+      "name" : "user1",
+      "type" : "Developer"
+    };
+    var user1Id = user.add(user1Obj);
+
+    var user2Obj = {
+      "name" : "user2",
+      "type" : "Developer"
+    };
+    var user2Id = user.add(user2Obj);
+
+    expect(user1Id).toBe(1);
+    expect(user2Id).toBe(2);
+
+  });
+
+  it('should let you delete user', function() {
+    var user1Obj = {
+      "name" : "user1",
+      "type" : "Developer"
+    };
+
+    var user1Id = user.add(user1Obj);
+
+    var user2Obj = {
+      "name" : "user2",
+      "type" : "Developer"
+    };
+
+    var user2Id = user.add(user2Obj);
+
+    expect(user.getAll().length).toBe(2);
+    user.delete(user2Id);
+
+    expect(user.getAll().length).toBe(1);
+    user.delete(user1Id);
+
+    expect(user.getAll().length).toBe(0);
+
+  });
+
+
+});
